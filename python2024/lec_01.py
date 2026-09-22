f21a75a7f4bb39944ace8037972cb5a0f634e14c
divyagoyal-coder <divya.goyal@sunstone.in>
2026-09-22 13:03:40 +0530

    Add files via upload

---
+++ python2024/lec_01.py
@@ -0,0 +1,45 @@
+# name = "Divya"    
+# age = 10
+# height = 5.7
+# alive = True
+# GENDER = "Female"
+
+# list : mutable
+# fruits = ["Mango", "Litchi", "Apple"]
+
+# Tuples : immutable
+# star = (2, 7);
+
+
+# conditional statement
+# score = 23
+# if score >80 and score<100 :
+#     print("A : You're Great with Soccer!")
+# elif score < 30:
+#     print("B : You should try Cricket!!")
+# else:
+#     print("C : You need to start over!!")
+
+
+
+# For Loops
+# for x in fruits:
+#     print(x)
+
+
+# for i in range (5):
+#     print(i)
+
+# while Loop
+# i=1
+# while (i<=10):
+#     print(f"2 x {i} = {2*i}")
+#     i+=1
+
+
+# Function Definition and Calling a Function
+# def greeting():
+#     print("Welcome to ALTA!!");
+
+
+# greeting();
